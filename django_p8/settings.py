72dd7007cc2d9aff4b826e95c18f51bdcffe6842
Stéphane LE BOURGEOIS <52003471+SLB974@users.noreply.github.com>
2022-04-08 05:26:18 +0400

    removing debug_toolbar settings

--- django_p8/settings.py
+++ django_p8/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-ALLOWED_HOSTS = ["nutella974.herokuapp.com","127.0.0.1"]
+ALLOWED_HOSTS = ["nutella974.herokuapp.com","127.0.0.1","testserver", "localhost"]
 
 
 
@@ -146,34 +146,36 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 if DEBUG:
     INTERNAL_IPS = ('127.0.0.1', 'localhost',)
 
-    MIDDLEWARE += (
-                'debug_toolbar.middleware.DebugToolbarMiddleware',
-                )
+    # MIDDLEWARE += (
+    #             'debug_toolbar.middleware.DebugToolbarMiddleware',
+    #             )
 
-    INSTALLED_APPS += (
-                'debug_toolbar',
-                )
+    # INSTALLED_APPS += (
+    #             'debug_toolbar',
+    #             )
 
 
-    DEBUG_TOOLBAR_PANELS = [
-                'debug_toolbar.panels.versions.VersionsPanel',
-                'debug_toolbar.panels.timer.TimerPanel',
-                'debug_toolbar.panels.settings.SettingsPanel',
-                'debug_toolbar.panels.headers.HeadersPanel',
-                'debug_toolbar.panels.request.RequestPanel',
-                'debug_toolbar.panels.sql.SQLPanel',
-                'debug_toolbar.panels.staticfiles.StaticFilesPanel',
-                'debug_toolbar.panels.templates.TemplatesPanel',
-                'debug_toolbar.panels.cache.CachePanel',
-                'debug_toolbar.panels.signals.SignalsPanel',
-                'debug_toolbar.panels.logging.LoggingPanel',
-                'debug_toolbar.panels.redirects.RedirectsPanel',
-                'debug_toolbar.panels.profiling.ProfilingPanel',
-    ]
+    # DEBUG_TOOLBAR_PANELS = [
+    #             'debug_toolbar.panels.versions.VersionsPanel',
+    #             'debug_toolbar.panels.timer.TimerPanel',
+    #             'debug_toolbar.panels.settings.SettingsPanel',
+    #             'debug_toolbar.panels.headers.HeadersPanel',
+    #             'debug_toolbar.panels.request.RequestPanel',
+    #             'debug_toolbar.panels.sql.SQLPanel',
+    #             'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    #             'debug_toolbar.panels.templates.TemplatesPanel',
+    #             'debug_toolbar.panels.cache.CachePanel',
+    #             'debug_toolbar.panels.signals.SignalsPanel',
+    #             'debug_toolbar.panels.logging.LoggingPanel',
+    #             'debug_toolbar.panels.redirects.RedirectsPanel',
+    #             'debug_toolbar.panels.profiling.ProfilingPanel',
+    # ]
 
-    DEBUG_TOOLBAR_CONFIG = {
-                'INTERCEPT_REDIRECTS': False,
-        }
+    # DEBUG_TOOLBAR_CONFIG = {
+    #             'INTERCEPT_REDIRECTS': False,
+    #     }
     
-    import mimetypes
-    mimetypes.add_type("application/javascript", ".js", True)
+    # import mimetypes
+    # mimetypes.add_type("application/javascript", ".js", True)
+
+TESTS_SHOW_BROWSER = True
